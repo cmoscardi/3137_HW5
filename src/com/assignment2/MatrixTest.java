ffff3f35bb7a91cbc5e51880bb14ec1701dafb14
Christian <clm2183@columbia.edu>
2012-04-15 13:28:06 -0400

    committing to transfer between comps

--- src/com/assignment2/MatrixTest.java
+++ src/com/assignment2/MatrixTest.java
@@ -5,26 +5,29 @@ import java.util.Scanner;
 
 public class MatrixTest {
 	public static void main(String[] args) throws Exception{
+		Node[] nodes = new Node[20];
 		File matrix = new File("graph.txt");
 		Scanner read = new Scanner(matrix);
 		while(read.hasNextLine()){
 			String x = read.nextLine();
 			String[] y = x.split(" ");
-			
+			Node n=null;;
+			int counter =0;
 			//awmagawd letter
 			if(y.length==1){
-				Node n = new Node();
+				n = new Node();
 				n.letter = y[0].charAt(0);
 				//next line is def coordinates
 				x = read.nextLine();
 				y = x.split(" ");
 				n.x = Integer.parseInt(y[0]);
-				n.y = Integer.parseInt(y[0]);
-				n.z = Integer.parseInt(y[0]);
+				n.y = Integer.parseInt(y[1]);
+				n.z = Integer.parseInt(y[2]);
+				nodes[counter++]=n;
 			}
 			//awmagawd 3 numbers
 			else{
-				
+				n.addNCoords(Integer.parseInt(y[0]),Integer.parseInt(y[1]),Integer.parseInt(y[2]));
 			}
 		}
 	}
--- src/com/assignment2/Node.java
+++ src/com/assignment2/Node.java
@@ -6,7 +6,19 @@ public class Node {
 	int z;
 	Node[] adjacents;
 	char letter;
+	int adjCount;
+	int[][] adjCoords;
 	public Node(){
+		adjacents = new Node[4];
+		int[][] adjCoords= new int[4][3];
+		adjCount = 0;
+	}
+	
+	public void addNCoords(int x, int y, int z){
+		adjCoords[adjCount][0]=x;
+		adjCoords[adjCount][1]=y;
+		adjCoords[adjCount][2]=z;
+		adjCount++;
 	}
 	
 	
